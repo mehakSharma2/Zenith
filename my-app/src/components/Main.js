1ef62ff4e75afe68a61533a45072d795869d481f
neha62003 <neharajput62003@gmail.com>
2024-10-19 18:03:34 +0530

    main page edit

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import img1 from './img1.png'
+import img1 from './image 5.jpeg'
 import img2 from './img2.jpg'
 import {Link} from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
@@ -26,7 +26,7 @@ const TypedComponent = () => {
    };
 
 export default function Main(props){
-     document.body.style.backgroundColor = "#f2e7d7"
+     document.body.style.backgroundColor = "white"
 
   const navigate = useNavigate();
 
@@ -43,7 +43,7 @@ export default function Main(props){
   
   return (
     <>
-    <nav className="bg-[#bb6851] text-white">
+    <nav className="bg-[#3E92CC] text-white">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <span className="self-center text-3xl font-semibold whitespace-nowrap text-shadow-lg dark:text-white">Zenith</span>
       <Link to='/login' className='hover:text-shadow-lg '>Login/SignUp</Link>
@@ -52,13 +52,13 @@ export default function Main(props){
   
     <div className='w-screen h-screen flex '>
         <div className='h-full w-1/2 '>
-          <h1 className='text-6xl ml-24 mt-56 text-[#95523f] text-shadow-lg'>Developer Roadmap</h1>
+          <h1 className='text-6xl ml-24 mt-56 text-[#3e92cc] text-shadow-lg'>Developer Roadmap</h1>
           <h3 className='text-2xl ml-44 mt-5 text-[#435554]'><TypedComponent /></h3>
           
           <div className='flex mt-32 space-x-32'>
 
-          <Link className='w-56 h-16 p-2 bg-[#bb6851] text-white text-center drop-shadow-2xl rounded-2xl ml-28 hover:text-shadow-lg' to='/Form'>Customise your own <strong >ROADMAP</strong></Link>
-          <Link onClick={nextPage} className='w-56 h-16 pt-5 bg-[#bb6851] drop-shadow-2xl  text-white text-center rounded-2xl hover:text-shadow-lg' to='/'>View ROADMAPS</Link>
+          <Link className='w-56 h-16 p-2 bg-[#3E92CC] text-white text-center drop-shadow-2xl rounded-2xl ml-28 hover:text-shadow-lg' to='/Form'>Customise your own <strong >ROADMAP</strong></Link>
+          <Link onClick={nextPage} className='w-56 h-16 pt-5 bg-[#3E92CC] drop-shadow-2xl  text-white text-center rounded-2xl hover:text-shadow-lg' to='/'>View ROADMAPS</Link>
 
           </div>
         </div>
